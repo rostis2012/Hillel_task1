4d22fa35d0139e952d2bee80d8c16a3db3d1995e
rostis2012 <rostis2012@gmail.com>
2022-10-10 19:42:42 +0300

    My first Homework

---
+++ main.py
@@ -0,0 +1,7 @@
+#from __future__ import braces #выводит ошибку SyntaxError: not a chance
+import this #выводит в консоль Zen of Python
+import antigravity #Открывает в браузере ссылку https://xkcd.com/353/
+import __hello__ #выводит в консоли 'Hello world!'
+#from __future__ import braces #выводит ошибку с просьбой разместить в начале кода SyntaxError: from __future__ imports must occur at the beginning of the file
+print('Hello world!')
+print('First HomeWork!')
